7f73a46e3b0b0e3b5716322cf2d807f2d80e1a02
Zack Rouch <zachary.rouch@student.swic.edu>
2024-12-08 21:12:25 -0600

    Add main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-// TODO: Do what must be done
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import ItemList from './components/ItemList';
+import AddItemForm from './components/AddItemForm';
+import stateManager from './state/stateManager';
+
+function App() {
+  const [state, setState] = React.useState(stateManager.getState());
+
+  React.useEffect(() => {
+    stateManager.subscribe(() => {
+      setState(stateManager.getState());
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <AddItemForm />
+      <ItemList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
