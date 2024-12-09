ae2b5d090436e03ba8267b815a703f074a653039
Zack Rouch <zachary.rouch@student.swic.edu>
2024-12-08 20:36:15 -0600

    Add components header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h1 className="text-xl">CRUD Application</h1>
+    </header>
+  );
+}
+
+export default Header;
