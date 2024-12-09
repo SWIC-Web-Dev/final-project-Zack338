019de92101c064ef8c90c307c77c7d547a22d27a
Zack Rouch <zachary.rouch@student.swic.edu>
2024-12-08 21:16:04 -0600

    Add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,14 @@ export default {
   },
   plugins: [],
 };
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
